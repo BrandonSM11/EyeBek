6bdd1bd1ca67ac983b192fbf14e971aebbf70b8f
estebanorozcoo <estebanorozcosoorio@gmail.com>
2025-12-12 10:11:52 -0500

    In progress

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,4 +1,3 @@
-
 export interface LoginFormData {
   email: string;
   password: string;
@@ -17,4 +16,42 @@ export interface UseLoginFormReturn {
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
   clearError: (field: keyof LoginFormErrors) => void;
+}
+
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+
+export interface LoginResponse {
+  token: string;
+  company: Company;
+}
+
+
+export interface Company {
+  id: number;
+  name: string;
+  email: string;
+  status: number;
+  phone?: string;
+  address?: string;
+}
+
+
+export interface CompanyRegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  address: string;
+}
+
+
+export interface ApiError {
+  message: string;
+  errors?: Record<string, string[]>;
+  title?: string;
 }
